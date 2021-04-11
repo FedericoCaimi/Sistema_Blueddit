10e27c2d354ba773d7e376fd2c2366f71e1a47c4
Federico Caimi <federlrpm@gmail.com>
2021-04-11 17:23:48 -0300

    base code for Blueddit system proyect

---
+++ Sistema_Blueddit_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sistema_Blueddit_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
