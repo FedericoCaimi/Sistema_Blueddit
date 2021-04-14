72e4ff4c28bd4e24c31f22fb67ccbe2fc4e5f8d0
Federico Caimi <federlrpm@gmail.com>
2021-04-14 00:46:36 -0300

    server accept many clients

--- SistemaBlueddit.Client/ClientProgram.cs
+++ SistemaBlueddit.Client/ClientProgram.cs
@@ -9,12 +9,41 @@ namespace SistemaBlueddit.Client
     {
         static void Main(string[] args)
         {
+            var exit = false;
+
             Console.WriteLine("Cliente se esta iniciando");
             var tcpClient = new TcpClient(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0));
             tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 50000);
-            Console.WriteLine("Conectado al server y voy a enviar el header");
-            SendHeader(tcpClient);
-            Console.ReadLine();
+
+            Console.WriteLine("Bienvenido al Sistema Blueddit");
+            Console.WriteLine("uno - envia comando uno");
+            Console.WriteLine("dos - salir");
+            try
+            {
+                while (!exit)
+                {
+                    var option = Console.ReadLine();
+                    switch (option)
+                    {
+                        case "uno":
+                            Console.WriteLine("Conectado al server y voy a enviar el header");
+                            SendHeader(tcpClient);
+                            break;
+                        case "dos":
+                            exit = true;
+                            tcpClient.GetStream().Close();
+                            tcpClient.Close();
+                            break;
+                        default:
+                            Console.WriteLine("Opcion invalida...");
+                            break;
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Se perdió la conexión con el servidor: " + e.Message);
+            }
         }
 
         public static void SendHeader(TcpClient connectedClient)
--- SistemaBlueddit.Server/ServerProgram.cs
+++ SistemaBlueddit.Server/ServerProgram.cs
@@ -2,18 +2,69 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace SistemaBlueddit.Server
 {
     public class ServerProgram
     {
+        public static bool _exit = false;
+        public static readonly List<TcpClient> ConnectedClients = new List<TcpClient>();
         static void Main(string[] args)
         {
             Console.WriteLine("Server esta iniciando...");
             var tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 50000);
-            tcpListener.Start(1);
-            var acceptedClient = tcpListener.AcceptTcpClient();
-            RecieveHeader(acceptedClient);
+            tcpListener.Start(10);
+
+            var threadServer = new Thread(()=> ListenForConnections(tcpListener));
+            threadServer.Start();
+            
+        }
+
+        private static void ListenForConnections(TcpListener tcpListener)
+        {
+            while (!_exit)
+            {
+                try
+                {
+                    var acceptedClient = tcpListener.AcceptTcpClient();
+                    ConnectedClients.Add(acceptedClient);
+                    Console.WriteLine("Acepte una nueva conexion");
+                    var threadClient = new Thread(() => HandleClient(acceptedClient));
+                    threadClient.Start();
+
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("El servidor está cerrándose...");
+                    _exit = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Saliendo del listen...");
+        }
+
+        private static void HandleClient(TcpClient acceptedClient)
+        {
+            try
+            {
+                while (!_exit)
+                {
+                    RecieveHeader(acceptedClient);
+                    
+                }
+                Console.WriteLine("Sali del while...");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Removing client....");
+                ConnectedClients.Remove(acceptedClient);
+            }
+            Console.WriteLine("Saliendo del thread cliente....");
         }
 
         public static void RecieveHeader(TcpClient connectedClient)
@@ -26,5 +77,6 @@ namespace SistemaBlueddit.Server
             Console.WriteLine("Header data length: " + header.DataLength);
             Console.WriteLine("Header file name length: " + header.FileNameLength);
         }
+
     }
 }
