134ed227dd2ff645c65ad46cafbf8863cbc42f29
frmartaran <frmartaran@gmail.com>
2021-04-19 23:38:28 -0300

    Corrected deserialize post, implemented post creation in memory

--- SistemaBlueddit.Domain/Post.cs
+++ SistemaBlueddit.Domain/Post.cs
@@ -21,17 +21,21 @@ namespace SistemaBlueddit.Domain
                 serializedTopics += topic.SerializeObejct() + ",";
             }
             serializedTopics = serializedTopics.Remove(serializedTopics.Length - 1);
-            return $"name:{Name},topics:[{serializedTopics}],creationDate:'{CreationDate.Year + "-" + CreationDate.Month + "-" + CreationDate.Day}'";
+            return $"{{name:{Name},topics:[{serializedTopics}],creationDate:'{CreationDate:yyyy-MM-dd}'}}";
         }
 
         public Post DeserializeObject(string objectToDeserialize)
         {
-            var curlyBracesPattern = new Regex("{|}");
-            var objectProperties = curlyBracesPattern.Replace(objectToDeserialize, "").Split(",");
+            var curlyBracesPattern = new Regex("^{|}$");
+            var objectsInListPattern = new Regex("(?<=\\[).*(?=\\])");
+            var objectsInList = objectsInListPattern.Match(objectToDeserialize).Value;
+            objectsInList = objectsInList.Replace(",", "|");
+            var objectWithoutInitialCurlyBrances = curlyBracesPattern.Replace(objectToDeserialize, "");
+            var objectProperties = objectsInListPattern.Replace(objectWithoutInitialCurlyBrances, objectsInList).Split(",");
             foreach (var property in objectProperties)
             {
                 var propertyName = property.Split(":")[0];
-                var propertyValue = property.Split(":")[1];
+                var propertyValue = property.Replace(propertyName + ":", "");
                 switch (propertyName)
                 {
                     case ("name"):
@@ -39,17 +43,19 @@ namespace SistemaBlueddit.Domain
                         break;
                     case ("topics"):
                         var topics = new List<Topic>();
-                        var straigthBracesPattern = new Regex("[|]");
-                        var serializedTopics = straigthBracesPattern.Replace(propertyValue, "").Split(",");
+                        var straigthBracesPattern = new Regex("\\[|\\]");
+                        var serializedTopics = straigthBracesPattern.Replace(propertyValue, "").Replace("}|{", "},{").Split(",");
                         foreach(var serializedTopic in serializedTopics)
                         {
-                            var topic = new Topic().DeserializeObject(serializedTopic);
+                            var topicString = serializedTopic.Replace("|",",");
+                            var topic = new Topic().DeserializeObject(topicString);
                             topics.Add(topic);
                         }
                         Topics = topics;
                         break;
                     case ("creationDate"):
-                        CreationDate = DateTime.ParseExact(propertyValue, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                        var creationDate = propertyValue.Replace("'", "");
+                        CreationDate = Convert.ToDateTime(creationDate);
                         break;
                     default:
                         throw new Exception("Objeto no es del tipo post");
