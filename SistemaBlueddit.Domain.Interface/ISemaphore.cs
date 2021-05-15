9dddfa367d0931d4c7edec482a2dd914c46373c4
frmartaran <frmartaran@gmail.com>
2021-05-15 01:03:55 -0300

    Added missing file

---
+++ SistemaBlueddit.Domain.Interface/ISemaphore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SistemaBlueddit.Domain.Interface
+{
+    public interface ISemaphoreSlim
+    {
+        SemaphoreSlim SemaphoreSlim { get; }
+    }
+}
