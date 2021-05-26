8887807fbdeabc0e4d3602c4a43c2371ef387255
Federico Caimi <federlrpm@gmail.com>
2021-05-25 23:52:18 -0300

    new LogServer web api

---
+++ SistemaBlueddit.LogServer/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SistemaBlueddit.Domain;
+
+namespace SistemaBlueddit.LogServer.Controllers
+{
+    [ApiController]
+    [Route("[post]")]
+    public class PostController : ControllerBase
+    {
+
+        private IPostLogic PostLogic;
+        public PostController(IPostLogic postLogic) : base()
+        {
+            this.PostLogic = postLogic;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<Log> postLogs = this.PostLogic.GetAll().ToList(); ;
+                return Ok(postLogs);
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
